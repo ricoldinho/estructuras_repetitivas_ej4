23cdcf30b08c813f3813a080e2cec78505642b7b
ricoldinho <manuelricolopez@liceolapaz.net>
2025-10-29 09:43:41 +0100

    avanzando ej4, ej5 corregido y bien subido

--- src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio4.java
+++ src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio4.java
@@ -8,21 +8,29 @@ public class Ejercicio4 {
         int numero_secreto = (int) (Math.random() * 100) + 1;
         System.out.println(numero_secreto);
         int numero_usuario;
-        int iteradora = 0;
+
         do{
             System.out.println("Adivina un número entre 1 y 100:");
             Scanner teclado = new Scanner(System.in);
+            //recogemos número usuario
             numero_usuario = teclado.nextInt();
+            boolean repetido = true;
 
 
             //recorrer un array (vector) unidimensional
             for(int i = 0; i<numeros_usados.length; i++){
+                //comprobamos si el número existe -> mensaje repeticion
                 if(numero_usuario == numeros_usados[i]){
                     System.out.println("El número ya ha sido usado. No es el número secreto");
+                    repetido = true;
                 }else{
-                    numeros_usados[i] = numero_usuario;
+                    repetido = false;
                 }
             }
+            if(!repetido){
+                //numeros_usados[i] = numero_usuario;
+            }
+
 
         }while(numero_secreto != numero_usuario);
 
--- src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio5.java
+++ src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio5.java
@@ -1,4 +1,36 @@
 package edu.rico.estructurasrepetitivas.ejercicio4;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        System.out.println("Iniciamos programa");
+        double acumuladorNotas = 0;
+        int contadoraNotas=0;
+        while(true){
+            Scanner kb = new Scanner(System.in);
+            double nota;
+            //do while que valida nota, si no es valida el flujo se queda dentro del do while
+            do{
+                System.out.println("Introduzca nota:");
+                //asignamos nota
+                nota = kb.nextDouble();
+                //si es -1 salimos del do while, tendríamos que salir del while tambien
+                if(nota == -1){
+                    break;
+                }
+                System.out.println("Introduzca -1 para parar la ejecucion");
+            }while(nota < 0 || nota > 10);
+            //salimos del while si es -1
+            if(nota == -1){
+                break;
+            }
+            //usamos para saber cuantas notas válidas son introducidas
+            contadoraNotas++;
+            acumuladorNotas += nota;
+            System.out.println("Nota introducida: " + nota + " => nota acumulada = " + acumuladorNotas);
+        }
+        //solo se ejecuta cuando salimos del while y calcula la media de notas
+        System.out.println("La media es: " + (acumuladorNotas/contadoraNotas));
+    }
 }
