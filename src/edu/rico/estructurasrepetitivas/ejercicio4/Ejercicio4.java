ccd5775edaf95c52f2fe0d46bd99eb30d56e06d6
ricoldinho <manuelricolopez@liceolapaz.net>
2025-11-04 09:01:16 +0100

    ejercicio 4 finalizado

--- src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio4.java
+++ src/edu/rico/estructurasrepetitivas/ejercicio4/Ejercicio4.java
@@ -1,38 +1,54 @@
 package edu.rico.estructurasrepetitivas.ejercicio4;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Ejercicio4 {
-    public static void main(String[] args) {
-        int[] numeros_usados = new int[3];
+    public static void main(String[] args) throws InterruptedException {
+        int[] numeros_usados = new int[100];
         int numero_secreto = (int) (Math.random() * 100) + 1;
         System.out.println(numero_secreto);
         int numero_usuario;
+        int it = 0;
 
         do{
             System.out.println("Adivina un número entre 1 y 100:");
             Scanner teclado = new Scanner(System.in);
             //recogemos número usuario
             numero_usuario = teclado.nextInt();
-            boolean repetido = true;
+            boolean repetido = false;
 
 
             //recorrer un array (vector) unidimensional
-            for(int i = 0; i<numeros_usados.length; i++){
+            for(int i = 0; i<numeros_usados.length; i++) {
                 //comprobamos si el número existe -> mensaje repeticion
-                if(numero_usuario == numeros_usados[i]){
-                    System.out.println("El número ya ha sido usado. No es el número secreto");
+                if (numero_usuario == numeros_usados[i]) {
                     repetido = true;
-                }else{
-                    repetido = false;
                 }
             }
-            if(!repetido){
-                //numeros_usados[i] = numero_usuario;
+            if(!repetido) {
+                numeros_usados[it] = numero_usuario;
+                it++;
+            }
+
+        }while((numero_secreto != numero_usuario) && (it < numeros_usados.length));
+
+        if(numero_secreto == numero_usuario){
+            System.out.println("Has acertado! el número secreto era: " + numero_secreto);
+            System.out.println("Has utilizado " + it + " intentos para acertar");
+            for(int i = 0; i<3; i++){
+                Thread.sleep(1000);
+                System.out.println("FELICIDADES");
             }
 
 
-        }while(numero_secreto != numero_usuario);
+        }else{
+            System.out.println("PROGRAMA FINALIZADO, USASTE " + numeros_usados.length + " NÚMEROS");
+        }
+        System.out.println("NUMEROS USADOS POR EL USUARIO");
+        for(int i = 0; i<numeros_usados.length; i++){
+            System.out.println(numeros_usados[i]);
+        }
 
 
 
